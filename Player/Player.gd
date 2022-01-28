6708fbaade338c45c52e303f538140bcb7c35bab
Yeonho Jang <siabard@gmail.com>
2022-01-28 10:06:04 +0900

    Tilemap (autotilemap)

--- Player/Player.gd
+++ Player/Player.gd
@@ -5,6 +5,8 @@ const MAX_SPEED = 100
 const FRICTION = 10
 var velocity = Vector2.ZERO
 
+onready var animationPlayer = $AnimationPlayer
+
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
@@ -12,10 +14,14 @@ func _physics_process(delta):
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
-		velocity += input_vector * ACCELERATION * delta
-		velocity = velocity.clamped(MAX_SPEED * delta)
+		if input_vector.x > 0:
+			animationPlayer.play("RunRight")
+		else:	
+			animationPlayer.play("RunLeft")
+		velocity = velocity.move_toward( input_vector * MAX_SPEED, ACCELERATION )
 		
 	else:
-		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+		animationPlayer.play("IdleRight")
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION )
 	
-	move_and_collide(velocity)
+	velocity = move_and_slide(velocity)
